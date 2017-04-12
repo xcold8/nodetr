d985f491a6aa1a016c471f98df75c5ea78f7677d
Family <Family>
2017-04-12 17:48:48 +0300

    list items now show up on nodejs console log

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 var path = require('path');
 var express = require('express');
 var app = express();
+var connect = require('connect');
+var http = require('http');
+
+
+// parse urlencoded request bodies into req.body
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+
 var json1 = [{"title":"Buy Milk","done":true},{"title":"Go to bank","done":false},{"title":"Win 10K jackpot in poker game","done":false}];
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res){
-	res.sendFile('/Users/haimayalon/nodetr/tasky.html');
+	res.sendFile('./public/index.html');
 });
 
 app.get('/api/todos', function (req, res) {
-	res.send(json1);
+	res.send(JSON.stringify(json1));
 });
 
 app.listen(3000, function (){
 	console.log('Listening on port 3000');
+});
+app.post('/api/todos/new', function(req, res){
+	udata = req.body;
+	console.log(udata);
 });
